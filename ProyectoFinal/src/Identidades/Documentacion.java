fb0ce5344128a50ee7bb3dd46be7d3d9145fce5d
fdani <fdani@Smelter-Le-Novo>
2023-08-04 17:51:23 -0400

    agregue getter y setter a clase Documentacion

--- ProyectoFinal/src/Identidades/Documentacion.java
+++ ProyectoFinal/src/Identidades/Documentacion.java
@@ -4,5 +4,19 @@ public class Documentacion {
 	private String tipo;
 	private int noDocumento;
 	
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	public int getNoDocumento() {
+		return noDocumento;
+	}
+	public void setNoDocumento(int noDocumento) {
+		this.noDocumento = noDocumento;
+	}
+	
 }
 
