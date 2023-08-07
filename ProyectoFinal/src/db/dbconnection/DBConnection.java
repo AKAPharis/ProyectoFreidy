a92dcf0fa77238f3a103a61ea69e00e744219e72
AKAPharis <132085671+AKAPharis@users.noreply.github.com>
2023-08-07 11:17:49 -0400

    Creacion de la clase DBConnection para la conexion a la BD

---
+++ ProyectoFinal/src/db/dbconnection/DBConnection.java
@@ -0,0 +1,27 @@
+package db.dbconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DBConnection {
+	//Cambiar la conexion a lo hora de usar el programa
+	private String conectionString = "\"jdbc:mysql://localhost:3306/proyectofinal\",\"root\",\"P15m16o19!";
+	private Connection connection = null ;
+	public DBConnection(){
+		try {
+			connection = DriverManager.getConnection(conectionString);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public Connection getConnection() {
+		return connection;
+	}
+	
+	public void closeConnection() {
+		connection = null;
+	}
+	
+}
