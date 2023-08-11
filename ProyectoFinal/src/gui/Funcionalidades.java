18d9930f1714f4e2acfee979b55d865b4a366f34
fdani <fdani@Smelter-Le-Novo>
2023-08-10 21:14:14 -0400

    terminando el diseño del incio de secion y agregando la clase para añadir funcionalidaders

---
+++ ProyectoFinal/src/gui/Funcionalidades.java
@@ -0,0 +1,10 @@
+package gui;
+
+import javax.swing.JOptionPane;
+
+public class Funcionalidades extends Builder {
+	
+	public static void regristrarMedico() {
+		JOptionPane.showMessageDialog(null, "ola");
+	}
+}
